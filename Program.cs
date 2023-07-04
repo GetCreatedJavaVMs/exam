582bf70cc68d289955c305195c75fbe3c38c261d
GetCreatedJavaVMs <130655015+GetCreatedJavaVMs@users.noreply.github.com>
2023-07-04 10:08:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using RomanClass;
+using System;
+
+// Малков Роман 22ИС
+
+namespace RomanMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store("Пятерочка", "Москва, ул.Полярная 32к1");
+
+            Product CocaCola = new Product("CocaCola", 12);
+            Product Fanta = new Product("Fanta", 10);
+            Product Lays = new Product("Lays", 33);
+
+            CocaCola.Show();
+            Fanta.Show();
+            Lays.Show();
+
+            store.Products.Add(CocaCola);
+            store.Products.Add(Fanta);
+            store.Products.Add(Lays);
+
+            Console.WriteLine();
+
+            store.Products.Sort();
+            store.ShowAll();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RomanClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+// Малков Роман 22ИС
+
+namespace RomanClass
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }        
+        public List<Product> Products = new List<Product>();
+        
+        // constructor
+        public Store(string Name, string Address)
+        {
+            this.Name    = Name;
+            this.Address = Address;
+        }
+
+        // show store info method
+        public void ShowAll()
+        {
+            Console.WriteLine($"В магазине \"{this.Name}\" по адрессу {this.Address} есть следующие товары: ");
+            foreach (Product product in this.Products)
+            {
+                Console.WriteLine($"{product.Name} ({product.Count}) шт.");
+            }
+        }
+    }
+
+    
+    public class Product : IComparable<Product>{
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+        // constructor
+        public Product(string Name, int Count)
+        {
+            this.Name  = Name;
+            this.Count = Count;
+        }
+
+        // show method for product
+        public void Show()
+        {
+            Console.WriteLine($"Товар: \"{this.Name}\" ({this.Count} шт.)");
+        }
+
+        // method for compare other product
+        public int CompareTo(Product otherProduct)
+        {
+            if(this.Count < otherProduct.Count) return -1;       
+            else if (this.Count > otherProduct.Count) return 1;
+            else return 0;
+        }
+
+    }
+
+}
